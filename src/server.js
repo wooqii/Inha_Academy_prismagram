bb30573ef1a68f8b06ad1b137f21ee9a199d5418
wooqii <cpawookie@gmail.com>
2020-02-09 16:55:43 +0900

    200209_API_3
    sendMail_Function_with_Nodemailer

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env")});
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import { sendSecretMail } from "./utils";
+
+sendSecretMail("daniel@ceight.io", "12345");
 
 const PORT = process.env.PORT || 4000;
 
